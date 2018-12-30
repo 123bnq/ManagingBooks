d485058635134e4425f7b97ef80cd558998a6d36
123bnq <33340665+123bnq@users.noreply.github.com>
2018-12-31 00:34:57 +0100

    Add Book class

---
+++ ManagingBooks/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagingBooks
+{
+    class Book
+    {
+        private int m_NoAuthor;
+        private int m_NoSignature;
+
+        public int Number { get; set; }
+        public string[] Signatures { get; set; }
+        public string Title { get; set; }
+        public string[] Authors { get; set; }
+        public string Publisher { get; set; }
+        public int Version { get; set; }
+        public int Year { get; set; }
+        public string DayBought { get; set; }
+        public int Pages { get; set; }
+        public string Price { get; set; }
+    }
+}
