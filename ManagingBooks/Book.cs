8fbda355491797066843f1518eda35053e51e5ec
123bnq <33340665+123bnq@users.noreply.github.com>
2019-01-01 01:45:19 +0100

    Revert "Add Author class + chage Book class"
    This reverts commit 67c4768e30ad5c4f4c5535960579f3685779cdc4.

--- ManagingBooks/Book.cs
+++ ManagingBooks/Book.cs
@@ -8,44 +8,18 @@ namespace ManagingBooks
 {
     class Book
     {
+        private int m_NoAuthor;
+        private int m_NoSignature;
+
         public int Number { get; set; }
         public string[] Signatures { get; set; }
         public string Title { get; set; }
-        public Author[] Authors { get; set; }
+        public string[] Authors { get; set; }
         public string Publisher { get; set; }
         public int Version { get; set; }
         public int Year { get; set; }
-        public string Medium { get; set; }
         public string DayBought { get; set; }
-        public string Place { get; set; }
         public int Pages { get; set; }
-        public double Price { get; set; }
-
-        public int NoSignature { get; }
-
-        public int NoAuthor { get; }
-
-        public static int Count = 0;
-
-        public Book()
-        {
-        }
-
-        public Book(int noAuthor, int noSignature, int number, string title, string publisher, int version, int year, string medium, string date, string place, int pages, double price)
-        {
-            NoAuthor = noAuthor;
-            NoSignature = noSignature;
-            Number = number;
-            Title = title;
-            Publisher = publisher;
-            Version = version;
-            Year = year;
-            Medium = medium;
-            DayBought = date;
-            Place = place;
-            Pages = pages;
-            Price = price;
-            Count++;
-        }
+        public string Price { get; set; }
     }
 }
