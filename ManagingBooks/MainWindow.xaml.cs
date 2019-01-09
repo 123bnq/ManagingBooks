537d2558bfae543407225886bfbeb3ae3a4ab34e
123bnq <33340665+123bnq@users.noreply.github.com>
2019-01-10 00:22:47 +0100

    Building Search window

--- ManagingBooks/MainWindow.xaml.cs
+++ ManagingBooks/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ManagingBooks.Model;
 using ManagingBooks.Windows;
 
 namespace ManagingBooks
@@ -24,6 +25,8 @@ namespace ManagingBooks
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = new SearchBookModel();
+            SearchList.ItemsSource = (DataContext as SearchBookModel).DisplayBooks;
         }
 
         private void AddWindow_Click(object sender, RoutedEventArgs e)
@@ -39,6 +42,19 @@ namespace ManagingBooks
         {
             e.CanExecute = true;
         }
+
+        private void SearchCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = !string.IsNullOrWhiteSpace(BoxSearchText.Text);
+        }
+
+        private void SearchCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            SearchBookModel context = (this.DataContext as SearchBookModel);
+
+            MessageBox.Show($"Search is executed\nSearch for: {context.SearchText}\nSearch By: {context.SearchBy}", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+            (this.DataContext as SearchBookModel).SearchText = string.Empty;
+        }
     }
 
     public static class CustomCommands
@@ -47,5 +63,7 @@ namespace ManagingBooks
         {
             new KeyGesture(Key.Q, ModifierKeys.Control)
         });
+
+        public static readonly RoutedUICommand Search = new RoutedUICommand("Search", "Search", typeof(CustomCommands));
     }
 }
---
+++ ManagingBooks/Model/SearchBookModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+
+namespace ManagingBooks.Model
+{
+    class SearchBookModel : INotifyPropertyChanged
+    {
+        private string m_SearchText;
+        private string m_SearchBy;
+
+        public string SearchText {
+            get => m_SearchText;
+            set
+            {
+                if (value != m_SearchText)
+                {
+                    m_SearchText = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+        public string SearchBy
+        {
+            get => m_SearchBy;
+            set
+            {
+                if (value != m_SearchBy)
+                {
+                    m_SearchBy = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public ObservableCollection<SearchBook> DisplayBooks { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+    
+    class SearchBook
+    {
+        public int Number { get; set; }
+        public string Signatures { get; set; }
+        public string Title { get; set; }
+        public string Authors { get; set; }
+        public int Version { get; set; }
+        public string Medium { get; set; }
+    }
+}
