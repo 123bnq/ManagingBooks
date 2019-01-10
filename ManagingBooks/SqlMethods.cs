f9b2440cc79e1ffe2bddcb359ca355a4a0a8a995
123bnq <33340665+123bnq@users.noreply.github.com>
2019-01-11 00:01:42 +0100

    Create separate static class to handle SQLite short-handed methods

---
+++ ManagingBooks/SqlMethods.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagingBooks
+{
+    static class SqlMethods
+    {
+        public static void SqlConnect(out SqliteConnection con)
+        {
+            con = new SqliteConnection("" + new SqliteConnectionStringBuilder
+            {
+                DataSource = "Data\\Database.db"
+            });
+            con.Open();
+        }
+    }
+}
