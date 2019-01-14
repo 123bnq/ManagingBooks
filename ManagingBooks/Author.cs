b4c609a7bb6c77364dea3be06b900e039911998f
123bnq <33340665+123bnq@users.noreply.github.com>
2019-01-15 00:43:52 +0100

    simplify Author class

--- ManagingBooks/Author.cs
+++ ManagingBooks/Author.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ManagingBooks
+﻿namespace ManagingBooks
 {
     class Author
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string Name { get; set; }
     }
 }
