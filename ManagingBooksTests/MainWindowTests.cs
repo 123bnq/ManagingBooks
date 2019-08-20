399ec8d5b53d0e6ad2f1c76e1100740c6a07a4f5
123bnq <33340665+123bnq@users.noreply.github.com>
2019-08-21 00:03:50 +0200

    Test project

---
+++ ManagingBooksTests/MainWindowTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ManagingBooks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagingBooks.Tests
+{
+    [TestClass()]
+    public class MainWindowTests
+    {
+        [TestMethod()]
+        public void SearchBookTest()
+        {
+            MainWindow mainWindow = new MainWindow(9);
+            mainWindow.SearchBook();
+            Assert.Fail();
+        }
+    }
+}
